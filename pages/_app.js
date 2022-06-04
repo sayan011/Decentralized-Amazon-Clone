051c1b38fb143c6566c4de5f7907622b691c85aa
Sayan Chowdhury <sayan.chowdhury.aiml25@heritageit.edu.in>
2022-06-04 09:45:50 +0530

    added Sidebar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { MoralisProvider } from "react-moralis";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER}
+      appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
+    >
+      <Component {...pageProps} />)
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
